2fcc594a15bda3aeea3989721eead8882bb794c1
Ernesto Hegi <ernesto.hegi@gmail.com>
2024-05-12 17:44:55 +0100

    animate player

--- scripts/MainCharacter.gd
+++ scripts/MainCharacter.gd
@@ -4,6 +4,10 @@ const SPEED = 100.0
 const DECELERATION = SPEED/8
 const ACCELERATION = SPEED/2
 
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get('parameters/playback')
+
 func _physics_process(_delta):
 	var vector = Vector2.ZERO;
 	
@@ -17,4 +21,12 @@ func _physics_process(_delta):
 	velocity.x = move_toward(velocity.x, SPEED * vector.x, ACCELERATION) if horizontalDirection else move_toward(velocity.x, 0, DECELERATION)
 	velocity.y = move_toward(velocity.y, SPEED * vector.y, ACCELERATION) if verticalDirection else move_toward(velocity.y, 0, DECELERATION)
 
+	if vector != Vector2.ZERO:
+		animation_tree.set('parameters/Run/blend_position', vector)
+		animation_tree.set('parameters/Idle/blend_position', vector)
+		animation_state.travel('Run')
+	else:
+		
+		animation_state.travel('Idle')
+	
 	move_and_slide()
