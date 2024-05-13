4b6a889d83052174d2c64a8043629391f54834eb
Ernesto Hegi <ernesto.hegi@gmail.com>
2024-05-13 19:22:46 +0100

    attack

--- scripts/MainCharacter.gd
+++ scripts/MainCharacter.gd
@@ -4,14 +4,30 @@ const SPEED = 100.0
 const DECELERATION = SPEED/8
 const ACCELERATION = SPEED/2
 
+enum {
+	MOVE,
+	ATTACK
+}
+
+var state = MOVE
+
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get('parameters/playback')
 
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(_delta):
+	match state:
+		MOVE:
+			move()
+		ATTACK:
+			attack()
+
+func move():
 	var vector = Vector2.ZERO;
-	
-	var horizontalDirection = Input.get_axis("ui_left", "ui_right")
-	var verticalDirection = Input.get_axis("ui_up", "ui_down")
+	var horizontalDirection = Input.get_axis("move_left", "move_right")
+	var verticalDirection = Input.get_axis("move_up", "move_down")
 	
 	vector.x = horizontalDirection
 	vector.y = verticalDirection
@@ -23,8 +39,20 @@ func _physics_process(_delta):
 	if vector != Vector2.ZERO:
 		animation_tree.set('parameters/Run/blend_position', vector)
 		animation_tree.set('parameters/Idle/blend_position', vector)
+		animation_tree.set('parameters/Attack/blend_position', vector)
 		animation_state.travel('Run')
 	else:
 		animation_state.travel('Idle')
-	
+
 	move_and_slide()
+
+	if (Input.is_action_just_pressed('attack')):
+		state = ATTACK
+
+func attack():
+	velocity = Vector2.ZERO
+	animation_state.travel('Attack')
+
+
+func attack_animation_finished():
+	state = MOVE
