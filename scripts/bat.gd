7b7f455f53598ec36b0f2228a231d734cbeb09f5
Ernesto Hegi <ernesto.hegi@gmail.com>
2024-05-27 21:15:45 +0100

    adds knockback

--- scripts/bat.gd
+++ scripts/bat.gd
@@ -7,16 +7,10 @@ extends CharacterBody2D
 const BAT_DEATH_EFFECT = preload("res://scenes/bat_death_effect.tscn")
 const SPEED = Vector2.ZERO
 
-enum {
-	IDLE,
-	WANDER,
-	CHASE
-}
-
 var is_alive = true
 var bat_death_effect = null 
 var main_scene = null
-var move = CHASE
+var is_stunned = false
 
 func _ready():
 	main_scene = get_tree().current_scene
@@ -25,26 +19,14 @@ func _ready():
 func _physics_process(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, 200 * delta)
 	
-	match move:
-		IDLE:
-			seek_player()
-			pass
-		WANDER:
-			pass
-		CHASE:
-			var player = player_detection_zone.player
-			
-			sprite.flip_h = velocity.x < 0
-			
-			if player != null:
-				var player_direction = player.global_position - global_position  # Should normalize the direction here
-				velocity = velocity.move_toward(player_direction, 200)
-			pass
-			
+	# Figure a better way to keep knockback and trigger chase
+	if !is_stunned && player_detection_zone.is_player_inside_detection_zone():
+		chase_player()
+	
 	move_and_slide()
 
-
 func _on_hurtbox_area_entered(area):
+	is_stunned = true
 	stats.health -= area.damage
 	velocity = area.knockback_vector * 120
 
@@ -55,7 +37,13 @@ func _on_stats_no_health():
 func add_death_animation():
 	main_scene.add_child(bat_death_effect)
 	bat_death_effect.global_position = global_position
+
+func chase_player():
+	var player = player_detection_zone.player
 	
-func seek_player():
-	if player_detection_zone.is_player_inside_detection_zone():
-		move = CHASE
+	sprite.flip_h = velocity.x < 0
+	
+	if player != null:
+		# Should normalize the direction here
+		var player_direction = player.global_position - global_position  
+		velocity = velocity.move_toward(player_direction, 200)	
